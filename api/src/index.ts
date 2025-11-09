4023620087ce7bcd38b1f8a5b1296a5f74160349
Deni Himawan <denihimawan595@gmail.com>
2025-11-09 15:17:33 +0700

    nambah cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import cors from "cors"
 
 dotenv.config();
 
@@ -6,11 +7,17 @@ import express from "express";
 import userRoutes from "./routes/userRoute"
 
 
+
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true
+}));
 
 app.use(express.json());
 
 app.use("/api/users", userRoutes)
-
 
 export default app;
