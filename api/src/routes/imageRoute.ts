158c246fd98dd9988f36bc1f8d1080f6ab216a3b
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-11-24 12:37:29 +0700

    update route

--- api/src/routes/imageRoute.ts
+++ api/src/routes/imageRoute.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import multer from "multer";
-import { uploadLostReportImage, uploadFoundReportImage } from "../controllers/imageController";
+import { uploadController } from "../controllers/imageController";
 
 const router = Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
 // Upload gambar lost report
-router.post("/lost-report/:id/upload", upload.single("image"), uploadLostReportImage);
+router.post("/lost-report/:id/upload", upload.single("image"), uploadController.uploadLostReportImage);
 
 // Upload gambar found report
-router.post("/found-report/:id/upload", upload.single("image"), uploadFoundReportImage);
+router.post("/found-report/:id/upload", upload.single("image"), uploadController.uploadFoundReportImage);
 
 export default router;
