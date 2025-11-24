4db31d54db7c6a7feafc64e9ba2ac8a14002b199
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-11-24 12:37:51 +0700

    update middleware

--- api/src/middlewares/roleMiddleware.ts
+++ api/src/middlewares/roleMiddleware.ts
@@ -1,25 +1,19 @@
 import { Request, Response, NextFunction } from "express";
+import { ResponseData } from "../utils/Response";
 
 export const authorizeRoles = (...allowedRoles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
     
-    return (req: Request, res: Response, next: NextFunction) => {
+    const user = (req as any).user;
 
-        const user = (req as any).user;
+    if (!user) {
+      return ResponseData.unauthorized(res, "Unauthorized");
+    }
 
-        if (!user) {
-            return res.status(401).json({
-                message: "unauthorized",
-                success: false,
-            });
-        }
+    if (!allowedRoles.includes(user.role)) {
+      return ResponseData.forbidden(res, "Role tidak memiliki akses");
+    }
 
-        if (!allowedRoles.includes(user.role)) {
-            return res.status(403).json({
-                message: "role tidak memiliki akses",
-                success: false,
-            });
-        }
-
-        next();
-    };
+    next();
+  };
 };
