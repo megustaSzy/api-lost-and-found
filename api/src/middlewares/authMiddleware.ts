7bf50e12f286995b5b61b2f6efcc442dfe43c441
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-13 21:43:11 +0700

    feat: add authService

--- api/src/middlewares/authMiddleware.ts
+++ api/src/middlewares/authMiddleware.ts
@@ -8,52 +8,29 @@ export const authMiddleware = async (
   res: Response,
   next: NextFunction
 ) => {
-  const authHeader = req.headers.authorization;
+  const token = req.cookies.accessToken;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  if (!token) {
     return ResponseData.unauthorized(res, "token tidak ditemukan");
   }
-
-  const token = authHeader.split(" ")[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
       id: number;
-      tokenId: string;
     };
 
-    // cek apakah tokenId masih ada (belum logout)
-    const tokenExists = await prisma.tb_accessToken.findUnique({
-      where: { token: decoded.tokenId },
-    });
-
-    if (!tokenExists) {
-      return ResponseData.forbidden(
-        res,
-        "token sudah tidak valid (sudah logout)"
-      );
-    }
-
-    // ambil data user
     const user = await prisma.tb_user.findUnique({
       where: { id: decoded.id },
-      select: { id: true, name: true, email: true, role: true, avatar: true },
+      select: { id: true, name: true, email: true, role: true },
     });
 
     if (!user) {
-      return ResponseData.notFound(res, "user tidak ditemukan");
+      return ResponseData.unauthorized(res, "User tidak ditemukan");
     }
 
     (req as any).user = user;
-    return next();
-  } catch (err: any) {
-    if (err.name === "TokenExpiredError") {
-      return ResponseData.unauthorized(
-        res,
-        "token expired, silakan login kembali"
-      );
-    }
-
-    return ResponseData.forbidden(res, "token tidak valid");
+    next();
+  } catch (err) {
+    return ResponseData.forbidden(res, "Token tidak valid atau sudah expired");
   }
 };
