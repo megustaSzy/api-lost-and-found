4632e8839547bde5a4970a681faa7859d73b8650
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-20 22:42:36 +0700

    refactor: rapihkan code

--- api/src/services/userService.ts
+++ api/src/services/userService.ts
@@ -4,68 +4,65 @@ import { createError } from "../utils/createError";
 import { UserData } from "../types/user";
 
 export const userService = {
+  // GET all
+  async getAllUsers() {
+    return prisma.tb_user.findMany({
+      where: { role: "User" },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        notelp: true,
+      },
+      orderBy: { id: "asc" },
+    });
+  },
+  // GET by ID
+  async getUserById(id: number) {
+    return prisma.tb_user.findUnique({
+      where: {
+        id,
+      },
+    });
+  },
 
-    // GET all
-    async getAllUsers() {
-        return prisma.tb_user.findMany({
-            where: { role: 'User' },
-                select: {
-                id: true,
-                name: true,
-                email: true,
-                role: true,
-                notelp: true
-            },
-            orderBy: { id: 'asc' }
-            });
-    },
-    // GET by ID
-    async getUserById(id: number) {
-        return prisma.tb_user.findUnique({
-            where: {
-                id
-            }
-        });
-    },
+  // update user by ID
+  async updateUserById(id: number, data: UserData) {
+    const user = await prisma.tb_user.findUnique({ where: { id } });
 
-    // update user by ID
-    async updateUserById(id: number, data: UserData) {
-        
-        const user = await prisma.tb_user.findUnique({
-            where: {
-                id
-            }
-        });
+    if (!user) {
+      throw createError("id tidak ditemukan", 404);
+    }
 
-        if(!user) createError("id tidak ditemukan", 404);
+    const updateData: Partial<UserData> = {};
 
-        if(data.password) {
-            data.password = await bcrypt.hash(data.password, 10);
-        }
+    if (data.name) updateData.name = data.name;
+    if (data.email) updateData.email = data.email;
 
-        return prisma.tb_user.update({
-            where: {
-                id
-            },
-            data
-        });
-    },
+    if (data.password) {
+      updateData.password = await bcrypt.hash(data.password, 10);
+    }
 
-    async deleteUserById(id: number) {
-        
-        const user = await prisma.tb_user.findUnique({
-            where: {
-                id
-            }
-        });
+    return prisma.tb_user.update({
+      where: { id },
+      data: updateData,
+    });
+  },
 
-        if(!user) createError("id tidak ditemukan", 404);
+  async deleteUserById(id: number) {
+    const user = await prisma.tb_user.findUnique({
+      where: {
+        id,
+      },
+    });
 
-        return prisma.tb_user.delete({
-            where: {
-                id
-            }
-        })
-    },
+    if (!user) createError("id tidak ditemukan", 404);
 
-}
+    return prisma.tb_user.delete({
+      where: {
+        id,
+      },
+    });
+  },
+};
