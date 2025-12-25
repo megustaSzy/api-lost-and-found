137a15490e47c9f9a274019b28f7d8dafaf9c3eb
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-25 13:26:48 +0700

    feat: fix loginWithGoogle

--- api/src/routes/authRoute.ts
+++ api/src/routes/authRoute.ts
@@ -1,14 +1,28 @@
 import { Router } from "express";
 
 import { authController } from "../controllers/authController";
+import passport from "passport";
 
 const router = Router();
 
 router.post("/register", authController.register);
-
 router.post("/login", authController.login);
 
-router.post("/refresh", authController.refreshToken); 
-router.post("/logout", authController.logout);        
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { session: false }),
+  authController.googleCallback
+);
+
+router.post("/refresh", authController.refreshToken);
+router.post("/logout", authController.logout);
+
+router.post("/forgot-password", authController.forgotPassword);
+router.get("/verify-reset", authController.verifyResetSession);
+router.post("/reset-password", authController.resetPassword);
 
 export default router;
